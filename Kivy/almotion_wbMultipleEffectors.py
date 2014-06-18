23e7997999b9be0da22e1d58288f8dd6b5a0da44
Mike McFarlane <mike@mikemcfarlane.co.uk>
2014-06-18 10:21:04 +0100

    Revert 1530c16..191e952
    This rolls back to commit 1530c165ae8e61536881c0c55083748748fb0773.

--- Kivy/almotion_wbMultipleEffectors.py
+++ Kivy/almotion_wbMultipleEffectors.py
@@ -24,7 +24,8 @@ def main(robotIP, PORT=9559):
     motionProxy.wakeUp()
 
     # Send robot to Stand Init
-    postureProxy.goToPosture("StandInit", 0.5)
+    id = postureProxy.post.goToPosture("Stand", 0.5)
+    postureProxy.wait(id, 0)
 
     # end go to Stand Init, begin initialize whole body
 
@@ -44,52 +45,70 @@ def main(robotIP, PORT=9559):
 
     # end initialize whole body, define arms motions
 
-    isAbsolute   = True
     useSensorValues = False
 
     # ***************************************************************
 
-    # Arms motion
-    effectorList = ["LArm"]
-    arm = "LArm"
-    axisDirection = 1    # +1 for LArm, -1 for RArm
+    # effectorList = ["LArm"]
+    # arm = "LArm"
+    # axisDirection = 1    # +1 for LArm, -1 for RArm
+    # frame = motion.FRAME_WORLD
+    # currentTf = motionProxy.getTransform(arm, frame, useSensorValues)
 
-    frame = motion.FRAME_TORSO
-    pathArm = []
+    for i in range(5):
 
-    currentTf = motionProxy.getTransform(arm, frame, useSensorValues)
 
-    # 1 - arm ready out front
-    target1Tf = almath.Transform(currentTf)
-    target1Tf.r1_c4 += 0.00 # x
-    target1Tf.r2_c4 += 0.00 * axisDirection # y
-    target1Tf.r3_c4 += 0.00 # z
 
-    # 2 - arm back
-    target2Tf = almath.Transform(currentTf)
-    target2Tf.r1_c4 -= 0.05
-    target2Tf.r2_c4 += 0.05
-    target2Tf.r3_c4 += 0.00
+        print i
 
-    # 3 - arm to ball using ball.y
-    target3Tf = almath.Transform(currentTf)
-    target3Tf.r1_c4 += 0.00
-    target3Tf.r2_c4 += 0.00 * axisDirection
-    target3Tf.r3_c4 += 0.00
+        # Arms motion
+        effectorList = ["LArm"]
+        arm = "LArm"
+        axisDirection = 1    # +1 for LArm, -1 for RArm
 
-    pathArm.append(list(target1Tf.toVector()))
-    pathArm.append(list(target2Tf.toVector()))
-    pathArm.append(list(target3Tf.toVector()))
+        frame = motion.FRAME_WORLD
+        pathArm = []
+        
+        currentTf = motionProxy.getTransform(arm, frame, useSensorValues)
 
-    pathList = [pathArm]
+        # 1 - arm ready out front
+        target1Tf = almath.Transform(currentTf)
+        target1Tf.r1_c4 += 0.05 # x
+        target1Tf.r2_c4 += 0.00 * axisDirection # y
+        target1Tf.r3_c4 += 0.00 # z
 
-    axisMaskList = [almath.AXIS_MASK_VEL]
+        # 2 - arm back
+        target2Tf = almath.Transform(currentTf)
+        target2Tf.r1_c4 += 0.00
+        target2Tf.r2_c4 += 0.15
+        target2Tf.r3_c4 += 0.15
 
-    coef = 1.5
-    timesList = [coef * (i + 1) for i in range(len(pathArm))]
+        # 3 - arm to ball using ball.y
+        target3Tf = almath.Transform(currentTf)
+        target3Tf.r1_c4 += 0.05
+        target3Tf.r2_c4 += 0.00 * axisDirection
+        target3Tf.r3_c4 += 0.10
 
-    # And move!
-    motionProxy.transformInterpolations(effectorList, frame, pathArm, axisMaskList, timesList)
+        pathArm.append(list(target1Tf.toVector()))
+        pathArm.append(list(target2Tf.toVector()))
+        pathArm.append(list(target3Tf.toVector()))
+
+        pathList = [pathArm]
+
+        axisMaskList = [almath.AXIS_MASK_VEL]
+
+        coef = 1.5
+        timesList = [coef * (i + 1) for i in range(len(pathArm))]
+
+        # And move!
+        id = motionProxy.post.transformInterpolations(effectorList, frame, pathArm, axisMaskList, timesList)
+        motionProxy.wait(id, 0)
+
+        # It is necessary to return the robot to the start position so the next target
+        # positions are not added to the last move position.
+        # id = postureProxy.post.goToPosture("Stand", 0.75)
+        # postureProxy.wait(id, 0)
+
 
     # ***************************************************************
 
@@ -209,7 +228,7 @@ def main(robotIP, PORT=9559):
     motionProxy.wbEnable(isEnabled)
 
     # Send robot to Pose Init
-    postureProxy.goToPosture("StandInit", 0.5)
+    postureProxy.goToPosture("Stand", 0.5)
 
     # Go to rest position
     motionProxy.rest()
